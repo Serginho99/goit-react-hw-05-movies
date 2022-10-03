7801941a1d6674d1fb1ea4c24d368963ea77435e
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-10-04 00:22:56 +0300

    restart style components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,17 @@
-import { NavLink } from 'react-router-dom';
-// import { NavList, NavListItem, NavListLink } from './NavBar.styled';
+// import { NavLink } from 'react-router-dom';
+import { NavList, NavListItem, NavListLink } from './NavBar.styled';
 
 export default function NavBar() {
   return (
     <>
-      <ul>
-        <li>
-          <NavLink to={'/'}>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/movies'}>Movies</NavLink>
-        </li>
-      </ul>
+      <NavList>
+        <NavListItem>
+          <NavListLink to={'/'}>Home</NavListLink>
+        </NavListItem>
+        <NavListItem>
+          <NavListLink to={'/movies'}>Movies</NavListLink>
+        </NavListItem>
+      </NavList>
     </>
   );
 }
